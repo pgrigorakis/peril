0c48e118c62e2df44a9eefc3f85dac533846756f
Panagiotis Grigorakis <p.grigorakis@gmail.com>
2026-04-03 22:31:19 +0200

    added function to encode game log messages into gob and publish to game logs queue

--- internal/pubsub/publish.go
+++ internal/pubsub/publish.go
@@ -1,15 +1,18 @@
 package pubsub
 
 import (
+	"bytes"
 	"context"
+	"encoding/gob"
 	"encoding/json"
 	"fmt"
+	"time"
 
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
 func PublishJSON[T any](ch *amqp.Channel, exchange, key string, val T) error {
-
 	valBytes, err := json.Marshal(val)
 	if err != nil {
 		return fmt.Errorf("could not marshal value: %w", err)
@@ -22,6 +25,31 @@ func PublishJSON[T any](ch *amqp.Channel, exchange, key string, val T) error {
 	if err != nil {
 		return fmt.Errorf("could not publish message: %w\n", err)
 	}
-
 	return nil
+}
+
+func PublishGob[T any](ch *amqp.Channel, exchange, key string, val T) error {
+	var valBytes bytes.Buffer
+	enc := gob.NewEncoder(&valBytes)
+	err := enc.Encode(val)
+	if err != nil {
+		return fmt.Errorf("could not encode value: %w", err)
+	}
+
+	return ch.PublishWithContext(context.Background(), exchange, key, false, false, amqp.Publishing{
+		ContentType: "application/gob",
+		Body:        valBytes.Bytes(),
+	})
+}
+
+func PublishGameLog(publichCh *amqp.Channel, username, msg string) error {
+	return PublishGob(
+		publichCh,
+		routing.ExchangePerilTopic,
+		routing.GameLogSlug+"."+username,
+		routing.GameLog{
+			Username:    username,
+			Message:     msg,
+			CurrentTime: time.Now(),
+		})
 }
